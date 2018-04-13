da663994a0549b41624a224d8b834063e53e48a9
idiv <915939795@qq.com>
2018-04-13 21:52:28 +0800

    test

--- pages/demo/page.js
+++ pages/demo/page.js
@@ -1,3 +1,15 @@
 Page({
+    onLoad() {
+        wx.getLocation({
+            type: 'wgs84',
+            success: function(res) {
+              var latitude = res.latitude
+              var longitude = res.longitude
+              var speed = res.speed
+              var accuracy = res.accuracy;
 
+              console.log(res);
+            }
+        })
+    }
 });
